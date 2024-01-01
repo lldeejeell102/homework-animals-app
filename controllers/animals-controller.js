341c8eb10b042363ad5eb4e3aa9bd13f41ab586b
Daniel Ji <daniel.ji0923@gmail.com>
2024-01-01 15:12:39 -0800

    added controllers

--- controllers/animals-controller.js
+++ controllers/animals-controller.js
@@ -11,38 +11,54 @@ const animals = require("../models/animals.js")
 ///////////////////////////////////////////////////////////////////////////////////////////
 // INDEX
 router.get("/", async (req, res) => {
-    await res.render("../views/index.ejs", {animals})
+    try{
+        await res.render("../views/index.ejs", {animals})
+    } catch {
+        console.log(err)
+    }
 })
 
 
 // NEW
 router.get("/new", async (req, res) => {
-    await res.render("../views/new.ejs")
+    try{
+        await res.render("../views/new.ejs")
+    } catch{
+        console.log(err)
+    }
 })
 
 
 // CREATE
 router.post("/", async (req,res) => {
-    const body = req.body
-    if (body.extinct === "on"){
-        body.extinct = true
-    } else {
-        body.extinct = false
+    try{
+        const body = req.body
+        if (body.extinct === "on"){
+            body.extinct = true
+        } else {
+            body.extinct = false
+        }
+        // add the animal to the array
+        animals.push(body)
+        // redirect them to the index page
+        await res.redirect("/animals")
+    } catch{
+        console.log(err)
     }
-    // add the animal to the array
-    animals.push(body)
-    // redirect them to the index page
-    await res.redirect("/animals")
 })
 
 
 // DESTROY
 router.delete("/:id", async (req,res) => {
-    const id = req.params.id
-    // arr.splice(index, numOfItemToCut)
-    animals.splice(id,1);
-    // redirects to index
-    await res.redirect("/animals")
+    try{
+        const id = req.params.id
+        // arr.splice(index, numOfItemToCut)
+        animals.splice(id,1);
+        // redirects to index
+        await res.redirect("/animals")
+    }catch{
+        console.log(err)
+    }
 })
 // test within curl
 // curl -X DELETE localhost:3000/fruits/0
@@ -50,31 +66,43 @@ router.delete("/:id", async (req,res) => {
 
 // EDIT
 router.get("/:id/edit", async (req,res) => {
-    const id = req.params.id
-    const animal = animals[id]
-    await res.render("../views/edit.ejs", {animal, id})
+    try{
+        const id = req.params.id
+        const animal = animals[id]
+        await res.render("../views/edit.ejs", {animal, id})
+    }catch{
+        console.log(err)
+    }
 })
 
 
 // UPDATE
 router.put("/:id", async (req,res) => {
-    const id = req.params.id
-    const body = req.body
-    if (body.extinct === "on"){
-        body.extinct = true
-    } else {
-        body.extinct = false
+    try{
+        const id = req.params.id
+        const body = req.body
+        if (body.extinct === "on"){
+            body.extinct = true
+        } else {
+            body.extinct = false
+        }
+        animals[id] = body
+        await res.redirect("/animals")
+    }catch{
+        console.log(err)
     }
-    animals[id] = body
-    await res.redirect("/animals")
 })
 
 
 // SHOW
 router.get("/:id", async (req, res) => {
-    const id = req.params.id
-    const animal = animals[id]
-    await res.render("../views/show.ejs", {animal, id})
+    try{
+        const id = req.params.id
+        const animal = animals[id]
+        await res.render("../views/show.ejs", {animal, id})
+    }catch{
+        console.log(err)
+    }
 })
 
 
